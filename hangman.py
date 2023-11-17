449121dfb40b70b8fd9b0b5d2a8c99b11d4c180f
Cyprian <Cyprian.Gibas@o2.pl>
2023-11-17 22:43:14 +0100

    renamed variables from Polish to English

--- hangman.py
+++ hangman.py
@@ -24,43 +24,43 @@ def draw_hangman(proba):
     print(hangman_stages[proba])
 
 def game():
-    print("Witaj w grze w wisielca!")
+    print("Welcome to hangman game!")
 
-    slowo_do_odgadniecia = choose_random_word().lower();
-    slowo_do_odgadniecia_set = set(slowo_do_odgadniecia)
-    odgadniete = set()
-    proba = 0
-    max_prob = 6
+    guessing_word = choose_random_word().lower();
+    guessing_word_set = set(guessing_word)
+    guessed = set()
+    attempt = 0
+    max_attempts = 6
 
     while True:
 
         if proba != 0:
-            draw_hangman(proba)
+            draw_hangman(attempt)
 
-        if proba == max_prob:
+        if proba == max_attempts:
             print("Przekroczyłeś maksymalną liczbę prób. Koniec gry.")
-            print("Poprawne słowo to: " + slowo_do_odgadniecia)
+            print("Poprawne słowo to: " + guessing_word)
             break
 
-        stan = "".join([litera if litera in odgadniete else "_" for litera in slowo_do_odgadniecia])
+        stan = "".join([litera if litera in guessed else "_" for litera in guessing_word])
         print(stan)
 
-        if set(stan) == slowo_do_odgadniecia_set:
-            print("Gratulacje! Zgadłeś słowo: " + slowo_do_odgadniecia)
+        if set(stan) == guessing_word:
+            print("Gratulacje! Zgadłeś słowo: " + guessing_word)
             break
 
         litera = input("Podaj literę: ").lower()
 
-        if litera in odgadniete:
+        if litera in guessed:
             print("Już podałeś literę " + litera + ". Spróbuj ponownie.")
             proba += 1
-            print("Pozostało prób: " + str(max_prob - proba))
-        elif litera in slowo_do_odgadniecia_set:
+            print("Pozostało prób: " + str(max_attempts - proba))
+        elif litera in guessing_word_set:
             print("Dobra robota! Podana litera jest w słowie.")
-            odgadniete.add(litera)
+            guessed.add(litera)
         else:
             print("Niestety, podana litera nie występuje w słowie.")
             proba += 1
-            print("Pozostało prób: " + str(max_prob - proba))
+            print("Pozostało prób: " + str(max_attempts - proba))
 
 game()
